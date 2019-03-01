642b0408835b0869277e75de655a1b7b2dc970f9
Katie Carrel <katiecarrel@comcast.net>
2019-02-28 17:18:17 -0800

    ha

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,34 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: [:show, :edit, :delete]
+  before_action :employee_params, only: [:create, :update]
+
+  def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    redirect_to @employee
+  end
+
+  def update
+    @employee = set_employee.update(employee_params)
+    redirect_to employees_path
+  end
+
+  private
+
+  def set_employee
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :office, :alias, :img_url, :title, :dog_id)
+  end
+
+
 end
